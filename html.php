45e02e5fb90dfb728d73902fff57ed19c279fffd
airen <airenliao@gmail.com>
2013-08-25 23:30:12 +0800

    add some files

---
+++ html.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Sass Flat UI</title>
+</head>
+<body>
+	
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include "html.php"; ?>
+
+<?php include "footer.php"; ?>
